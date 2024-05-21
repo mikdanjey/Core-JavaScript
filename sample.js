bcce4bf28b3137455ff5116d620027407d73ee96
Mikdan Jey <mikdanjey@gmail.com>
2024-05-21 15:31:07 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,1 +1,4 @@
 
+content = content.replace(/\\/g, '') // Remove all backslashes
+                .replace(/"/g, '') // Remove all double quotes
+                .replace(/>"/g, '>'); // Remove all >" sequences
